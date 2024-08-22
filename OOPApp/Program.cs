1e4677663ba156b20d2f4e9a287cf02d257513e7
EldrichMaid <kupo@mail.ru>
2024-08-22 12:09:24 +0300

    Класс без конструктора

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -1,11 +1,9 @@
 ﻿namespace OOPApp
 {
-    internal class Program
-    {
-        class Human
+    class Human
         {
             // Поля класса
-            public string? name;
+            public string name;
             public int age;
 
             // Метод класса
@@ -14,8 +12,8 @@
                 Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
             }
         }
-
-        struct Animal
+    
+    struct Animal
         {
             // Поля структуры
             public string type;
@@ -28,9 +26,20 @@
                 Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
             }
         }
+    internal class Program
+    {
+        
+        
         static void Main(string[] args)
         {
-            
+            Human human = new Human();
+            human.Greetings();
+            human.name = "Дмитрий";
+            human.age = 23;
+            human.Greetings();
+
+
+            Console.ReadKey();
         }
     }
 }
