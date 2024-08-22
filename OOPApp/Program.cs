1d0f45440918beb1e0321798e5b58190509bb049
EldrichMaid <kupo@mail.ru>
2024-08-22 11:57:06 +0300

    Добавьте файлы проекта.

---
+++ OOPApp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOPApp
+{
+    internal class Program
+    {
+        class Human
+        {
+            // Поля класса
+            public string? name;
+            public int age;
+
+            // Метод класса
+            public void Greetings()
+            {
+                Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+            }
+        }
+
+        struct Animal
+        {
+            // Поля структуры
+            public string type;
+            public string name;
+            public int age;
+
+            // Метод структуры
+            public void Info()
+            {
+                Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
