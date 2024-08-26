c43d96a1a281ce310b366bd487659768c52d1a30
EldrichMaid <kupo@mail.ru>
2024-08-26 13:59:18 +0300

    6.5.2 Добавлены классы круга,треугольника и квадрата

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -145,6 +145,47 @@
         }
     }
 
+    class Circle
+    {
+        public double radius;
+
+        public double Square()
+        {
+        }
+
+        public double Length()
+        {
+        }
+    }
+
+    class Triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public double Square()
+        {
+        }
+
+        public double Perimeter()
+        {
+        }
+    }
+
+    class Square
+    {
+        public int side;
+
+        public double Square()
+        {
+        }
+
+        public double Perimeter()
+        {
+        }
+    }
+
     struct Animal
         {
             // Поля структуры
