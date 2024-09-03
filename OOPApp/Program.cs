d78cfc061f1921be93f621ebc8ed50b466ce54e0
EldrichMaid <kupo@mail.ru>
2024-09-03 12:02:58 +0300

    7.1.5 Добавлены последовательные наследования классов

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -356,6 +356,22 @@
         private string ProgrammingLanguage;
     }
 
+    class Food { }
+
+    class Vegetable : Food { }
+
+    class Fruit : Food { }
+
+    class Apple : Fruit { }
+
+    class Banana : Fruit { }
+
+    class Pear : Fruit { }
+
+    class Potato : Vegetable { }
+
+    class Carrot : Vegetable { }
+
     internal class Program
     {
         static Department GetCurrentDepartment()
