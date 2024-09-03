9c97cc66b615f0e1b6cee9bd77c624742be70895
EldrichMaid <kupo@mail.ru>
2024-09-03 12:57:17 +0300

    7.1.10 Добавлен класс "BaseClass" и его наследник с зависимыми конструкторами

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -409,6 +409,30 @@
         }
     }
 
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+        public int Counter;
+        public DerivedClass(string name, string description): base(name) 
+        {
+            Description = description;
+        }
+        public DerivedClass(string name, string description, int counter) : this(name, description)
+        {
+            Counter = counter;
+        }
+    }
+
     internal class Program
     {
         static Department GetCurrentDepartment()
