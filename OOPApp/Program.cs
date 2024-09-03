333061b4f67ad13cb99d0d994ca0ffc579572da0
EldrichMaid <kupo@mail.ru>
2024-09-03 11:56:38 +0300

    7.1.4 Создан класс "Employee" и его наследники

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -339,6 +339,23 @@
 
     }
 
+    class Employee
+    {
+        public string Name;
+        public int Age;
+        public int Salary;
+    }
+
+    class ProjectManager : Employee
+    {
+        public string ProjectName;
+    }
+
+    class Developer : Employee
+    {
+        private string ProgrammingLanguage;
+    }
+
     internal class Program
     {
         static Department GetCurrentDepartment()
