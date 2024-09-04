90fdff707e7e0025b9a8c421a2f955d62c15f50f
EldrichMaid <kupo@mail.ru>
2024-09-04 14:11:35 +0300

    7.2.14 Добавлен класс с индексатором "IndexingClass"

--- OOPApp/Program.cs
+++ OOPApp/Program.cs
@@ -674,6 +674,27 @@
         }
     }
 
+    class IndexingClass
+    {
+        private int[] array;
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+
     internal class Program
     {
         static Department GetCurrentDepartment()
